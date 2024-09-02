aa467a094d2afd4a63deb8b37d95109d88b242ff
Sourya Mishra <sourya.mishra@thoughtworks.com>
2024-09-02 23:24:57 +0530

    create course service using fetch

--- src/app/services/courses-fetch.service.ts
+++ src/app/services/courses-fetch.service.ts
@@ -1,14 +1,45 @@
-import {Injectable} from "@angular/core";
-import {environment} from "../../environments/environment";
-import {Course} from "../models/course.model";
-
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Course } from '../models/course.model';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root',
 })
 export class CoursesServiceWithFetch {
-
   env = environment;
 
+  async loadAllCourses(): Promise<Course[]> {
+    const response = await fetch(`${this.env.apiRoot}/courses`);
+    const payload = await response.json();
+    return payload.courses as Course[];
+  }
 
+  async createCourse(course: Omit<Course, 'id'>): Promise<Course> {
+    const response = await fetch(`${this.env.apiRoot}/courses`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(course),
+    });
+    return response.json();
+  }
+
+  async updateCourse(course: Course): Promise<Course> {
+    const response = await fetch(`${this.env.apiRoot}/courses/${course.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(course),
+    });
+    return response.json();
+  }
+
+  async deleteCourse(courseId: string): Promise<Course> {
+    const response = await fetch(`${this.env.apiRoot}/courses/${courseId}`, {
+      method: 'DELETE',
+    });
+    return response.json();
+  }
 }
