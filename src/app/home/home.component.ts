c8690c4df3b0e7e71382928c1720d6ce348ac432
Sourya Mishra <sourya.mishra@thoughtworks.com>
2024-09-02 23:25:55 +0530

    use course service and display courses

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,64 @@
-import {Component, computed, effect, inject, Injector, signal} from '@angular/core';
-import {CoursesService} from "../services/courses.service";
-import {Course, sortCoursesBySeqNo} from "../models/course.model";
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
-import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
-import {MatDialog} from "@angular/material/dialog";
-import {MessagesService} from "../messages/messages.service";
-import {catchError, from, throwError} from "rxjs";
-import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
+import {
+  afterNextRender,
+  Component,
+  computed,
+  effect,
+  inject,
+  Injector,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { CoursesService } from '../services/courses.service';
+import { Course, sortCoursesBySeqNo } from '../models/course.model';
+import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { CoursesCardListComponent } from '../courses-card-list/courses-card-list.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MessagesService } from '../messages/messages.service';
+import { catchError, from, throwError } from 'rxjs';
+import {
+  toObservable,
+  toSignal,
+  outputToObservable,
+  outputFromObservable,
+} from '@angular/core/rxjs-interop';
+import { CoursesServiceWithFetch } from '../services/courses-fetch.service';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [
-    MatTabGroup,
-    MatTab,
-    CoursesCardListComponent
-  ],
+  imports: [MatTabGroup, MatTab, CoursesCardListComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  courses = signal<Course[]>([]);
+  beginnerCourses = computed(() => {
+    return this.courses().filter((course) => course.category === 'BEGINNER');
+  });
+  advancedCourses = computed(() => {
+    return this.courses().filter((course) => course.category === 'ADVANCED');
+  });
 
+  coursesService = inject(CoursesService);
+
+  constructor() {
+    afterNextRender(() => {
+      console.log('After next render');
+    });
+    console.log('constructor');
+    this.loadAllCourses();
+  }
+
+  ngOnInit(): void {
+    console.log('on init');
+  }
+
+  async loadAllCourses() {
+    try {
+      const courses = await this.coursesService.loadAllCourses();
+      this.courses.set(courses.sort(sortCoursesBySeqNo));
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
