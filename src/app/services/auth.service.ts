930fbc41710b644d23bbf1bbafb477ceeef081fc
Sourya Mishra <sourya.mishra@thoughtworks.com>
2024-09-03 18:28:27 +0530

    implement authentication

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,14 +1,46 @@
-import {computed, effect, inject, Injectable, signal} from "@angular/core";
-import {User} from "../models/user.model";
-import {environment} from "../../environments/environment";
-import {Router} from "@angular/router";
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
+import { User } from '../models/user.model';
+import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 const USER_STORAGE_KEY = 'user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private userSignal = signal<User | null>(null);
+  user = this.userSignal.asReadonly();
+  isLoggedIn = computed(() => Boolean(this.user()));
+  env = environment;
+  httpClient = inject(HttpClient);
+  router = inject(Router);
 
+  constructor() {
+    this.loadUserFromStorage();
+  }
 
+  loadUserFromStorage() {
+    const user = localStorage.getItem(USER_STORAGE_KEY);
+    if (user) this.userSignal.set(JSON.parse(user));
+  }
+
+  async login(email: string, password: string): Promise<User> {
+    const response$ = this.httpClient.post<User>(`${this.env.apiRoot}/login`, {
+      email,
+      password,
+    });
+    const user = await firstValueFrom(response$);
+    this.userSignal.set(user);
+    localStorage.setItem(USER_STORAGE_KEY, JSON.stringify(user));
+    return user;
+  }
+
+  async logout() {
+    this.userSignal.set(null);
+    localStorage.removeItem(USER_STORAGE_KEY);
+    await this.router.navigateByUrl('/login');
+  }
 }
